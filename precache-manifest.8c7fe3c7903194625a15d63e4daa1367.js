2b0cb821961fccb8fcdec8cda44ed37d9e5d4b17
linkonf <useforedubylitugou@gmail.com>
2022-07-04 11:06:30 +0000

    xxx

--- precache-manifest.8c7fe3c7903194625a15d63e4daa1367.js
+++ precache-manifest.8c7fe3c7903194625a15d63e4daa1367.js
@@ -1,167 +1,167 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6099aa46586b755603aaa0ef610083da",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/additional-page.yml.dist"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/additional-page.yml.dist"
   },
   {
     "revision": "3bcaaf53a63bc09dd9c81dfd012d84ea",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/config.yml.dist"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/config.yml.dist"
   },
   {
     "revision": "83a0b5926c3193bf35a4777d822b3b6f",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/config.yml.dist.sample-sui"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/config.yml.dist.sample-sui"
   },
   {
     "revision": "fd699694515ebb6d8f133017dd42dbaa",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/custom.css.sample"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/custom.css.sample"
   },
   {
     "revision": "3747a299c93ac18efb842e9c77321eae",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/icons/favicon-16x16.png"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/icons/favicon-16x16.png"
   },
   {
     "revision": "3f34d7c8357fc288f219a789362384bb",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/icons/favicon-32x32.png"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/icons/favicon-32x32.png"
   },
   {
     "revision": "e5da3a77211431da43847bcad73c4d28",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/icons/icon-any.png"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/icons/icon-any.png"
   },
   {
     "revision": "f26b71a48b6d7b638fe480d280fa2e59",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/icons/icon-any.svg"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/icons/icon-any.svg"
   },
   {
     "revision": "71d635e1641b6aa7b4418e584b209117",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/icons/icon-maskable.png"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/icons/icon-maskable.png"
   },
   {
     "revision": "733524d70dd5748e4973841831752a72",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/icons/safari-pinned-tab.svg"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/icons/safari-pinned-tab.svg"
   },
   {
     "revision": "299503dbb36a775a2f7dd2a3648e114a",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/manifest.json"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/manifest.json"
   },
   {
     "revision": "2310e486d38639bd762c521ecfff5673",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/tools/sample.png"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/tools/sample.png"
   },
   {
     "revision": "251284a7da3dbe13b758d4939254e95c",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/assets/tools/sample2.png"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/assets/tools/sample2.png"
   },
   {
     "revision": "ec90cb99c09832515da8",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/app.c14294d7.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/app.c14294d7.css"
   },
   {
     "revision": "670f44aa559ba4160478",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-0183b1fa.7a249bfc.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-0183b1fa.7a249bfc.css"
   },
   {
     "revision": "219f6404e94733f640cd",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-0e326c80.500fe474.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-0e326c80.500fe474.css"
   },
   {
     "revision": "74f6f1ed34a44668cee1",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-190df574.108fa8e3.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-190df574.108fa8e3.css"
   },
   {
     "revision": "9fe112ce2926c9fb5f70",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-1daf6107.3610de1b.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-1daf6107.3610de1b.css"
   },
   {
     "revision": "bbd8e695e3d9c25f82b2",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-25e44120.4733196c.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-25e44120.4733196c.css"
   },
   {
     "revision": "a4f8660481d87c547361",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-25efb736.3f75b2e0.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-25efb736.3f75b2e0.css"
   },
   {
     "revision": "0c0c8667a9fda7c0b43f",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-4cfdcfc7.b74aac1e.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-4cfdcfc7.b74aac1e.css"
   },
   {
     "revision": "dc4adc500f81044df299",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-513e4b73.d4907178.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-513e4b73.d4907178.css"
   },
   {
     "revision": "4000be622883d5e2ffaa",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-54c1ae82.29a37b88.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-54c1ae82.29a37b88.css"
   },
   {
     "revision": "dd97f3527a08d6d70bd3",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-68f308f4.97912d31.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-68f308f4.97912d31.css"
   },
   {
     "revision": "8999743f16557a0267f9",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-77038566.89da3e06.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-77038566.89da3e06.css"
   },
   {
     "revision": "f83f3b1d76d260d25bc0",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-99176266.b69211c5.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-99176266.b69211c5.css"
   },
   {
     "revision": "5c9bd4345a33d9705e8c",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-cb9c851c.193204e9.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-cb9c851c.193204e9.css"
   },
   {
     "revision": "6f5cf887cf8a22c3215b",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-fd3ed72e.dce59c38.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-fd3ed72e.dce59c38.css"
   },
   {
     "revision": "a9c763104c5663300ae6",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/css/chunk-vendors.97cf1437.css"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/css/chunk-vendors.97cf1437.css"
   },
   {
     "revision": "a78ffbbed2d858c61e068e3b756c9988",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-brands-400.a78ffbbe.ttf"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-brands-400.a78ffbbe.ttf"
   },
   {
     "revision": "cd2b4095e9ce66cde642c3502a4022d9",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-brands-400.cd2b4095.woff2"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-brands-400.cd2b4095.woff2"
   },
   {
     "revision": "b1a1bebb63656b34a23982706f712f71",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-regular-400.b1a1bebb.ttf"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-regular-400.b1a1bebb.ttf"
   },
   {
     "revision": "e8a1ba418ee6d897d1339ef22e6d8e60",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-regular-400.e8a1ba41.woff2"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-regular-400.e8a1ba41.woff2"
   },
   {
     "revision": "55b416a8df21f9f987aa352f10d1343b",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-solid-900.55b416a8.woff2"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-solid-900.55b416a8.woff2"
   },
   {
     "revision": "738201559a50502aacabdbdb02720910",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-solid-900.73820155.ttf"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-solid-900.73820155.ttf"
   },
   {
     "revision": "0d6f5f1852affca569715821fe4aa29b",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-v4compatibility.0d6f5f18.ttf"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-v4compatibility.0d6f5f18.ttf"
   },
   {
     "revision": "786e6b3373bab47e928c81c26eeccb08",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/fa-v4compatibility.786e6b33.woff2"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/fa-v4compatibility.786e6b33.woff2"
   },
   {
     "revision": "b4d2c4c39853ee244272c04999b230ba",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/lato-v16-latin-regular.b4d2c4c3.woff2"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/lato-v16-latin-regular.b4d2c4c3.woff2"
   },
   {
     "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/lato-v16-latin-regular.b8ee546a.woff"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/lato-v16-latin-regular.b8ee546a.woff"
   },
   {
     "revision": "43c849ea0258ce0d23a480e840881f16",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/raleway-v14-latin-regular.43c849ea.woff2"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/raleway-v14-latin-regular.43c849ea.woff2"
   },
   {
     "revision": "60b344eb8dd676754364fc5ae4500d62",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/fonts/raleway-v14-latin-regular.60b344eb.woff"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/fonts/raleway-v14-latin-regular.60b344eb.woff"
   },
   {
     "revision": "304e93cc175518d0bb76e5e96e93fd12",
@@ -169,82 +169,82 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "ec90cb99c09832515da8",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/app.a3b09982.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/app.a3b09982.js"
   },
   {
     "revision": "670f44aa559ba4160478",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-0183b1fa.565503b9.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-0183b1fa.565503b9.js"
   },
   {
     "revision": "219f6404e94733f640cd",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-0e326c80.20d457c6.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-0e326c80.20d457c6.js"
   },
   {
     "revision": "1ad8fb41bf915b68592a",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-11d7ef3d.0fcce8a7.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-11d7ef3d.0fcce8a7.js"
   },
   {
     "revision": "74f6f1ed34a44668cee1",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-190df574.92884379.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-190df574.92884379.js"
   },
   {
     "revision": "9fe112ce2926c9fb5f70",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-1daf6107.0358e820.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-1daf6107.0358e820.js"
   },
   {
     "revision": "bbd8e695e3d9c25f82b2",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-25e44120.b2a4f499.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-25e44120.b2a4f499.js"
   },
   {
     "revision": "a4f8660481d87c547361",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-25efb736.b65fe034.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-25efb736.b65fe034.js"
   },
   {
     "revision": "0c0c8667a9fda7c0b43f",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-4cfdcfc7.3ca75189.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-4cfdcfc7.3ca75189.js"
   },
   {
     "revision": "dc4adc500f81044df299",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-513e4b73.c63c573f.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-513e4b73.c63c573f.js"
   },
   {
     "revision": "4000be622883d5e2ffaa",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-54c1ae82.a7af3574.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-54c1ae82.a7af3574.js"
   },
   {
     "revision": "dd97f3527a08d6d70bd3",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-68f308f4.50dab410.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-68f308f4.50dab410.js"
   },
   {
     "revision": "8999743f16557a0267f9",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-77038566.d7546948.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-77038566.d7546948.js"
   },
   {
     "revision": "6c589a6df1b73b697809",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-96e93ae4.873d4b0c.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-96e93ae4.873d4b0c.js"
   },
   {
     "revision": "f83f3b1d76d260d25bc0",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-99176266.fe45e200.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-99176266.fe45e200.js"
   },
   {
     "revision": "5c9bd4345a33d9705e8c",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-cb9c851c.8d2905af.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-cb9c851c.8d2905af.js"
   },
   {
     "revision": "6f5cf887cf8a22c3215b",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-fd3ed72e.b3d67756.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-fd3ed72e.b3d67756.js"
   },
   {
     "revision": "a9c763104c5663300ae6",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/js/chunk-vendors.f9635b76.js"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/js/chunk-vendors.f9635b76.js"
   },
   {
     "revision": "471c3507cf3400229a66d6682ce45529",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/logo.png"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/logo.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer/robots.txt"
+    "url": "https://cdn.jsdelivr.net/gh/linkonf/homer@master/robots.txt"
   }
 ]);
